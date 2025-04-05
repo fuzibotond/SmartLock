8b60eae68612b61b571c81adc4e6e9282ecbd8c7
Füzi Botond <fuziboti@gmail.com>
2025-04-05 12:19:59 +0200

    Modify run command

--- main.py
+++ main.py
@@ -75,7 +75,9 @@ def get_logs():
     return {"logs": "csecs"}
 
 
-# ✅ Run FastAPI Server
-if __name__ == "__main__":
+import os
 
-    uvicorn.run(app, port=8000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
